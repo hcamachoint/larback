73cfe4d274efb4e48303e3a674ef63fb55a6e482
Hugo Camacho <hcamacho.int@gmail.com>
2021-01-11 12:39:36 -0400

    feat: products & categories

---
+++ database/migrations/2021_01_11_141604_create_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name')->comment('Name');
+            $table->string('description')->comment('Description');
+            $table->decimal('price', 8, 2)->comment('Price');
+            $table->unsignedBigInteger('category_id')->comment('Category identifier');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
